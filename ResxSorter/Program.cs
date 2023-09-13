89c3eb505581f2bc10be8a0b8a05f300c6bb879c
Kevin Bost <kitokeboo@gmail.com>
2023-09-13 00:46:45 -0700

    Fixing force option

--- ResxSorter/Program.cs
+++ ResxSorter/Program.cs
@@ -33,7 +33,8 @@ public sealed class Program
         {
             new CliCommand("no-op"),
             inputFileOption,
-            outputFileOption
+            outputFileOption,
+            forceOption
         };
 
         rootCommand.SetAction((ParseResult parseResult) =>
